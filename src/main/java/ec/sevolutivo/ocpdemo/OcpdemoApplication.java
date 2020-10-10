b313ad80119f11adffa77c6f0eca1596c7dcbe9e
Jorge Aguilar Bravo <jorgeaguilar@softwareevolutivo.com.ec>
2020-10-10 14:30:07 -0500

    Initial commit

---
+++ src/main/java/ec/sevolutivo/ocpdemo/OcpdemoApplication.java
@@ -0,0 +1,13 @@
+package ec.sevolutivo.ocpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcpdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ec/sevolutivo/ocpdemo/OcpdemoController.java
@@ -0,0 +1,15 @@
+package ec.sevolutivo.ocpdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OcpdemoController {
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hi " + name;
+    }
+
+}
